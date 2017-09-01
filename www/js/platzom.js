3f8389aaa8c8c766357946acd9f2d7d811141dc1
JenniferGalarza <jennyglez2@hotmail.com>
2017-09-01 13:30:03 -0600

    iniciando

--- www/js/platzom.js
+++ www/js/platzom.js
@@ -15,20 +15,44 @@ if(str.toLowerCase().startsWith(`z`)){
 
 
 
-console.log(platzom("programar"));
-console.log(platzom("Zorro"));
-console.log<(platzom("renacuajillos"))
-
 /*si la palabra traducida tiene 10 o mas letras se debe partir a la mitad y unir con un guion */
 
-var length = str.length;
-if (length >=1){
-    translation = str.slice(0, Math.round(length/2)+ `-` + str.slice(Math.round(length/2)));
+/*var length = str.length;
+if (length >=10){
+    translation = str.slice(0, Math.round(length/2))+ `-` + str.slice(Math.round(length/2));
     
 }
 return translation;
+}*/
+let length = translation.length;
+if(length >= 10){
+    const fHalf = translation.slice(0, Math.round(length/2));
+    const sHalf = translation.slice(Math.round(length/2));
+    translation = `$(fHalf)-$(sHalf)`;
 }
-
 
 /*si la palabra original es un palindromo, ninguna de las anteriores regglas funciona y se devuelve la palabra
 intercalando entre minusculas y mayusculas */
+
+const reverse = (str)=> str.split('').reverse.join('');
+function minMay(str){
+    const l = str.length;
+    let translation = "";
+    let capitalize = true;
+    for(let i = 0; i < length; i++){
+        const char = str.charAt(i);
+        translation += capitalize ? char.toUpperCase() : char.toLowerCase();
+    }
+}
+
+if(str = reverse(str)){
+    return minMay(str);
+}
+
+
+return translation;
+}
+
+console.log(platzom("programar"));
+console.log(platzom("Zorro"));
+console.log(platzom("abecedarios"));
